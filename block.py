08b33de0cfb78a161f9fbe9cc60364c8fd0dbea6
Riley Paul <rileypaul96@gmail.com>
2023-05-12 13:37:39 -0700

    slight change to spacing

--- block.py
+++ block.py
@@ -4,9 +4,14 @@ from dataclasses import dataclass
 class Block:
   beg: float
   end: float
+  gov: str = "beg" 
+  # Governing dimension determining order
+  # Can be mid, beg, or end
 
   def __post_init__(self):
     self.mid = (self.beg + self.end)/2
     self.len = abs(self.end - self.beg)
 
-  
+  def dist_other(self,other):
+    getattr(self,self.gov)
+    getattr(other,self.gov)
